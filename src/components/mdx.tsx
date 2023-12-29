65e67c083e1961555940da305028f21a471f19d7
Armel Talla <armel@clipsai.com>
2023-12-28 22:40:01 -0600

    remove feedback

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -12,10 +12,7 @@ export { CodeGroup, Code as code, Pre as pre } from '@/components/Code'
 export function wrapper({ children }: { children: React.ReactNode }) {
 	return (
 		<article className="flex h-full flex-col pb-10 pt-16">
-			<Prose className="flex-auto">{children}</Prose>
-			<footer className="mx-auto mt-16 w-full max-w-2xl lg:max-w-5xl">
-				<Feedback />
-			</footer>
+			<Prose className="flex-auto mb-16">{children}</Prose>
 		</article>
 	)
 }
