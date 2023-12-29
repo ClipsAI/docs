fd7c49a73dae6bdbdd4756389ce66c15110e52fe
Armel Talla <armel@clipsai.com>
2023-12-29 09:33:55 -0600

    update copyright in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -119,7 +119,7 @@ function SmallPrint() {
     return (
         <div className="flex flex-col items-center justify-between gap-5 border-t border-zinc-900/5 pt-8 dark:border-white/5 sm:flex-row">
             <p className="text-xs text-zinc-600 dark:text-zinc-400">
-                &copy; Copyright {new Date().getFullYear()}. All rights reserved.
+                &copy; {new Date().getFullYear()} Clips AI, Inc. All rights reserved.
             </p>
             <div className="flex gap-4">
                 <SocialLink href="https://github.com/ClipsAI/clipsai" icon={GitHubIcon}>
