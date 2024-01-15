f9faa5d68bdd1a59572841e19917f6447545769d
Benjamin Smidt <benjamin.smidt@utexas.edu>
2024-01-15 11:12:49 -0600

    Update Transcribe documentation

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -122,3 +122,48 @@ export function Property({
 		</li>
 	)
 }
+
+
+export function Parameters({ children }: { children: React.ReactNode }) {
+	return (
+		<div className="my-6">
+			<ul
+				role="list"
+				className="m-0 max-w-[calc(theme(maxWidth.lg)-theme(spacing.8))] list-none divide-y divide-zinc-900/5 p-0 pl-6 dark:divide-white/5"
+			>
+				{children}
+			</ul>
+		</div>
+	)
+}
+
+
+export function Parameter({
+	name,
+	children,
+	type,
+}: {
+	name: string
+	children: React.ReactNode
+	type?: string
+}) {
+	return (
+		<li className="m-0 px-0 py-4 first:pt-0 last:pb-0">
+			<dl className="m-0 flex flex-wrap items-center gap-x-3 gap-y-2">
+				<dt className="sr-only">Name</dt>
+				<dd>
+                    <strong className="mr-2">{name}</strong>
+				{type && (
+						<span className="font-mono text-xs text-zinc-400 dark:text-zinc-500">
+							{type}
+						</span>
+				)}
+                </dd>
+				<dt className="sr-only">Description</dt>
+				<dd className="w-full flex-none [&>:first-child]:mt-0 [&>:last-child]:mb-0">
+					{children}
+				</dd>
+			</dl>
+		</li>
+	)
+}
